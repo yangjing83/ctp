3ca490a8a0603a40c349ec5f763a12bbfe0b27bf
Huifeng_Yang <yaghf2000@163.com>
2021-04-30 08:56:34 +0800

    update

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -1,7 +1,25 @@
+import com.optioninfo.jctp.ctp.*;
 
 public class TestApi {
 
     public static void main(String[] args) {
+
+        String path = "/data/ctp-flow-path/";
+        CThostFtdcMdApi mdApi = CThostFtdcMdApi.CreateFtdcMdApi(path);
+        CThostFtdcMdSpi mdSpi = new CThostFtdcMdSpi();
+
+        mdApi.RegisterSpi(mdSpi);
+
+        System.out.println(mdApi.GetTradingDay());
+
+        CThostFtdcReqUserLoginField field = new CThostFtdcReqUserLoginField();
+        mdApi.ReqUserLogin(field, 1);
+
+        // 订阅行情
+        mdApi.SubscribeMarketData(new String[]{"rb2110"}, 1);
+
+        mdApi.Init();
+        mdApi.Join();
 
     }
 
